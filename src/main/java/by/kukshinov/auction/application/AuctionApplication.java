8311c082ace7daa9bfaeacc153e91ffa83009e4c
Андрей <andrekukshinov@gmail.com>
2020-10-30 20:55:36 +0300

    init smelly code

--- src/main/java/by/kukshinov/auction/application/AuctionApplication.java
+++ src/main/java/by/kukshinov/auction/application/AuctionApplication.java
@@ -1,41 +1,29 @@
 package by.kukshinov.auction.application;
 
+import by.kukshinov.auction.data.DataException;
+import by.kukshinov.auction.data.FileDataReader;
+import by.kukshinov.auction.data.ItemFileDataReader;
+import by.kukshinov.auction.data.ParticipantFileDataReader;
 import by.kukshinov.auction.model.Auction;
 import by.kukshinov.auction.model.Item;
 import by.kukshinov.auction.model.Participant;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
 public class AuctionApplication {
 
-    public static final int FIRST_ITEM_ID = 1;
-    public static final int SECOND_ITEM_ID = 2;
-    public static final String VASE = "Vase";
-    public static final String CANDLE = "Candle";
-    public static final BigDecimal FIRST_PRICE = new BigDecimal(450);
-    public static final BigDecimal SECOND_PRICE = new BigDecimal(455);
 
-    public static final int FIRST_PARTICIPANT_ID = 1;
-    public static final int SECOND_PARTICIPANT_ID = 2;
-    public static final BigDecimal FIRST_CAPITAL = new BigDecimal(23450);
-    public static final BigDecimal SECOND_CAPITAL = new BigDecimal(54455);
+    public static final String ITEMS_JSON = "src/json/items.json";
+    public static final String PARTICIPANTS_JSON = "src/json/participants.json";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataException {
 	   Auction auction = Auction.getInstance();
-	   Item vase = new Item(FIRST_ITEM_ID, VASE, FIRST_PRICE);
-	   Item candle = new Item(SECOND_ITEM_ID, CANDLE, SECOND_PRICE);
-	   Item lamp = new Item(3, "lamp", new BigDecimal("5633"));
-	   List<Item> expectedItems = Arrays.asList(vase, candle, lamp);
-	   Participant firstParticipant = new Participant(FIRST_PARTICIPANT_ID,
-			 FIRST_CAPITAL);
-	   Participant secondParticipant = new Participant(SECOND_PARTICIPANT_ID,
-			 SECOND_CAPITAL);
-	   Participant thirdParticipant = new Participant(3,
-			 new BigDecimal(74455));
-	   List<Participant> expectedParticipants = Arrays
-			 .asList(firstParticipant, secondParticipant, thirdParticipant);
+	   ObjectMapper objectMapper = new ObjectMapper();
+	   FileDataReader<Item> itemDataReader = new ItemFileDataReader(objectMapper);
+	   FileDataReader<Participant> participantFileDataReader = new ParticipantFileDataReader(objectMapper);
+	   List<Item> expectedItems = itemDataReader.readData(ITEMS_JSON);
+	   List<Participant> expectedParticipants = participantFileDataReader.readData(PARTICIPANTS_JSON);
 	   auction.setParticipants(expectedParticipants);
 	   auction.setItems(expectedItems);
 	   auction.startAuctionBidding();
